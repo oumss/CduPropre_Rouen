3976c1faba5784f98433f44ac756e08a8ead728d
oumss <oumaima.moutawadii@gmail.com>
2023-04-12 00:17:05 +0200

    navbar

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,7 @@
 import { View, Text, SafeAreaView, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { UserIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon } from "react-native-heroicons/outline"
 
 
 const Home = () => {
@@ -17,16 +18,16 @@ const Home = () => {
     <SafeAreaView>
       <Text>
         {/* Header */}
-        <View>
-          <Image className="h-7 w-7 bg-gray-300 p-4 rounded-full" source={{uri: 'https://links.papareact.com/wru' }} />
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image className="h-8 w-8 bg-gray-300 p-4 rounded-full" source={{uri: 'https://links.papareact.com/wru' }} />
           <View>
-            <Text>Cdupropre</Text>
-            <Text>Current location</Text>
+            <Text className="font-bold text-gray-400 text-xs">Current Location</Text>
+            <Text className="font-bold text-xl">C du propre
+            <ChevronDownIcon size={20} color="#00CCBB"/>
+            </Text>
           </View>
         </View>
-
       </Text>
-
     </SafeAreaView>
   )
 }
