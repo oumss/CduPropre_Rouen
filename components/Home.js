90477c141f480c87475cd634b66ac0b5ac2f3e01
oumss <oumaima.moutawadii@gmail.com>
2023-04-24 23:24:20 +0200

    Callout of trashes

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,10 @@
-import { View, Text, SafeAreaView, Image, TextInput } from 'react-native';
+import { View, Text, SafeAreaView, Image, TextInput, TouchableOpacity } from 'react-native';
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { UserIcon, ChevronDownIcon, SearchIcon, CameraIcon, AdjustmentsVerticalIcon, HomeIcon, PlusCircleIcon, CurrencyDollarIcon, UserCircleIcon } from "react-native-heroicons/outline";
-import {GOOGLE_MAPS_APIKEY} from "@env";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 import Map from './Map';
+import Report from './Report';
 
 const Home = () => {
 
@@ -11,36 +12,37 @@ const Home = () => {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: "Accueil",
-      headerShown : false
+      headerShown: false
     })
   }, []);
 
   return (
-    
+
     <SafeAreaView className="h-full bg-white pt-5">
       <Text>
 
         {/* Header */}
         <View className="flex-row pb-2 items-center mx-4 space-x-2">
           <Image className=" flex-1 h-11 w-11 p-4" source={require('./img/logo_cote.png')} />
-          
+
           <View className="flex-row mx-4 ">
             <Text className="font-bold text-gray-400 text-sm">Localisation : Rouen </Text>
           </View>
+
           <View className=" mx-4 ">
-            <Image className="h-8 w-8 bg-gray-300 p-4 mx-4 rounded-full" source={{url: 'https://links.papareact.com/wru' }} />
+            <Image className="h-8 w-8 bg-gray-300 p-4 mx-4 rounded-full" source={{ url: 'https://links.papareact.com/wru' }} />
             <Text className="font-bold text-gray-400 text-xs">oumss78 </Text>
           </View>
-        </View>
-        </Text>
-        
-        <View className="h-5/6 ">
-
-            <Map/>
 
         </View>
+      </Text>
 
-        {/* Search 
+      {/* MapView */}
+      <View className="h-5/6 ">
+        <Map />
+      </View>
+
+      {/* Search 
         <View className='flex-row items-center space-x-2 pb-2 mx-4'>
            <View className='flex-row space-x-2 flex-1 bg-gray-200 p-3'>
             <CameraIcon color="#00CCBB" />
@@ -63,33 +65,38 @@ const Home = () => {
           <AdjustmentsVerticalIcon color="#00CCBB"/>
         </View>
           */}
-      
+
 
 
       {/* Footer */}
       <View className="flex-row pt-4 px-6 items-center space-x-5">
-        <View className="flex-1 pt-1 items-center h-12 w-1 bg-[#B390EC] rounded-full ">
-            <HomeIcon size={40} color="#000" />
-        </View>
 
-        <View className="flex-1 pt-1 items-center h-12 w-1 bg-[#ACC687] rounded-full ">
-            <PlusCircleIcon size={40} color="#000" />
-        </View>
+        <TouchableOpacity className="flex-1 pt-1 items-center h-12 w-1 bg-[#B390EC] rounded-full ">
+          <HomeIcon size={40} color="#000" />
+        </TouchableOpacity>
 
-        <View className="flex-1 pt-1 items-center h-12 w-1 bg-[#ACC687] rounded-full ">
-            <CurrencyDollarIcon size={40} color="#000" />
-        </View>
+        <TouchableOpacity className="flex-1 pt-1 items-center h-12 w-1 bg-[#ACC687] rounded-full "
+          onPress={() =>{
+            console.log("hey")
+          }} 
+        >
+          <PlusCircleIcon size={40} color="#000" />
+        </TouchableOpacity>
 
-        <View className="flex-1 pt-1 items-center h-12 w-1 bg-[#ACC687] rounded-full ">
-            <UserCircleIcon size={40} color="#000" />
-        </View>
+        <TouchableOpacity className="flex-1 pt-1 items-center h-12 w-1 bg-[#ACC687] rounded-full ">
+          <CurrencyDollarIcon size={40} color="#000" />
+        </TouchableOpacity>
+
+        <TouchableOpacity className="flex-1 pt-1 items-center h-12 w-1 bg-[#ACC687] rounded-full ">
+          <UserCircleIcon size={40} color="#000" />
+        </TouchableOpacity>
 
       </View>
-      
+
     </SafeAreaView>
 
 
-    
+
   )
 }
 
--- components/Map.js
+++ components/Map.js
@@ -1,19 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, {useState, useEffect} from 'react'
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React, { useState, useEffect, useLayoutEffect } from 'react'
+import MapView, { Callout, Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native';
+
 
 const Map = () => {
+
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [pin, setPin] = useState({
-      latitude: 49.063850,
-      longitude: 2.09200548,
+    latitude: 49.063850,
+    longitude: 2.09200548,
   })
 
   useEffect(() => {
     (async () => {
-      
+
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         console.log('Permission to access location was denied');
@@ -32,46 +37,73 @@ const Map = () => {
     })();
   }, []);
 
-  
+
 
   return (
-  
-      <MapView
-            className="flex-1"
-            mapType='mutedStandard'
-            initialRegion={{
-                latitude: 49.063850,
-                longitude: 2.09200548,
-                latitudeDelta:0.005,
-                longitudeDelta:0.005,
-            }}
-            showsUserLocation={true}
-            showsMyLocationButton={true}
-            onUserLocationChange={(e) => {
-              //console.log("onUserLocationChange", e.nativeEvent.coordinate);
-              setPin({
-                latitude: e.nativeEvent.coordinate.latitude,
-                longitude: e.nativeEvent.coordinate.longitude,
-                latitudeDelta: e.nativeEvent.coordinate.latitudeDelta,
-                longitudeDelta: e.nativeEvent.coordinate.longitudeDelta,
-              })
-            }}
-            showsCompass={true}
-            
-          >
-            <Marker
-            coordinate={{
-              latitude: 49.063850,
-              longitude: 2.09200578,
-            }}
-            pinColor="#ACC687"
 
-            onPress={(e) => {
-              console.log("Touché");
-            }}
-        />
-      </MapView>
-    
+    <MapView
+      className="flex-1"
+      mapType='mutedStandard'
+      initialRegion={{
+        latitude: 49.063850,
+        longitude: 2.09200548,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+      showsUserLocation={true}
+      showsMyLocationButton={true}
+      onUserLocationChange={(e) => {
+        //console.log("onUserLocationChange", e.nativeEvent.coordinate);
+        setPin({
+          latitude: e.nativeEvent.coordinate.latitude,
+          longitude: e.nativeEvent.coordinate.longitude,
+          latitudeDelta: e.nativeEvent.coordinate.latitudeDelta,
+          longitudeDelta: e.nativeEvent.coordinate.longitudeDelta,
+        })
+      }}
+      showsCompass={true}
+
+    >
+      <Marker
+        coordinate={{
+          latitude: 49.063850,
+          longitude: 2.09200578,
+        }}
+        pinColor="#ACC687"
+
+      // onPress={(e) => {
+      //   console.log("Touché");
+
+
+      // }}
+      >
+        <Callout tooltip>
+          <View className="flex flex-col bg-[#acc687bd] h-72 w-72 rounded-xl shadow-2xl items-center">
+            <Text className="text-xl font-semibold text-gray-700">Déchet signalé</Text>
+            <Image className=" flex-1 h-2/3 w-2/3  " source={require('./img/trashes/IMG-20230407-WA0034.jpg')} />
+
+            <View className='flex-row items-center pt-1 '>
+              <View className="bg-[#bcc687] rounded-3xl ">
+                <Text className="text-l font-semibold ml-2 mr-2">Catégorie :</Text>
+              </View>
+              <Text className="text-l font-medium ml-2">Papier</Text>
+            </View>
+
+            <View className='flex-row items-center pt-1 '>
+              <View className="bg-gray-400 rounded-3xl ">
+                  <Text className="text-l font-semibold ml-2 mr-2">Signalé depuis :</Text>
+                </View>
+              <Text className="text-l font-semibold ml-2">20/03/2023</Text>
+            </View>
+
+            <TouchableOpacity className="bg-[#B390EC] h-10 w-48 mb-1 mt-1 rounded-full shadow-xl shadow-gray-400 items-center align-middle">
+              <Text className="text-l font-semibold pt-2">Ramasser le déchet</Text>
+            </TouchableOpacity>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
+
   )
 }
 
