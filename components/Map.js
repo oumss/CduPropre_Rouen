fd9ca079d68dbe5a1b3e1f512b78c3015bfd0999
oumss <oumaima.moutawadii@gmail.com>
2023-04-30 17:02:10 +0200

    Camera zone

--- components/Map.js
+++ components/Map.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, Button } from 'react-native'
 import React, { useState, useEffect, useLayoutEffect } from 'react'
 import MapView, { Callout, Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native';
+import { PROVIDER_GOOGLE } from 'react-native-maps';
+import { ArrowsPointingInIcon } from 'react-native-heroicons/outline';
 
 
 const Map = () => {
@@ -42,16 +44,21 @@ const Map = () => {
   return (
 
     <MapView
+
+      provider={MapView.PROVIDER_GOOGLE}
       className="flex-1"
-      mapType='mutedStandard'
+      mapType="mutedStandard"
       initialRegion={{
-        latitude: 49.063850,
-        longitude: 2.09200548,
+        latitude: 49.4498772,
+        longitude: 1.0344115,
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       }}
+
       showsUserLocation={true}
       showsMyLocationButton={true}
+      showsCompass={true}
+
       onUserLocationChange={(e) => {
         //console.log("onUserLocationChange", e.nativeEvent.coordinate);
         setPin({
@@ -61,49 +68,66 @@ const Map = () => {
           longitudeDelta: e.nativeEvent.coordinate.longitudeDelta,
         })
       }}
-      showsCompass={true}
 
     >
       <Marker
         coordinate={{
-          latitude: 49.063850,
-          longitude: 2.09200578,
+          latitude: 49.4498572,
+          longitude: 1.0344115,
         }}
         pinColor="#ACC687"
 
       // onPress={(e) => {
       //   console.log("Touché");
-
-
       // }}
+      
       >
         <Callout tooltip>
-          <View className="flex flex-col bg-[#acc687bd] h-72 w-72 rounded-xl shadow-2xl items-center">
+          <View className="flex flex-col bg-slate-100 h-72 w-72 rounded-3xl shadow-black shadow-xl items-center">
             <Text className="text-xl font-semibold text-gray-700">Déchet signalé</Text>
-            <Image className=" flex-1 h-2/3 w-2/3  " source={require('./img/trashes/IMG-20230407-WA0034.jpg')} />
+            <Image className=" flex-1 h-60 w-2/3 rounded-3xl " source={require('./img/trashes/WhatsAppImage.jpg')} />
 
             <View className='flex-row items-center pt-1 '>
               <View className="bg-[#bcc687] rounded-3xl ">
                 <Text className="text-l font-semibold ml-2 mr-2">Catégorie :</Text>
               </View>
-              <Text className="text-l font-medium ml-2">Papier</Text>
+              <Text className="text-l font-medium ml-2">Emballage plastique</Text>
             </View>
  
             <View className='flex-row items-center pt-1 '>
-              <View className="bg-gray-400 rounded-3xl ">
+              <View className="bg-[#acc687bd] rounded-3xl ">
                   <Text className="text-l font-semibold ml-2 mr-2">Signalé depuis :</Text>
                 </View>
-              <Text className="text-l font-semibold ml-2">20/03/2023</Text>
+              <Text className="text-l font-semibold ml-2">20 mars 2023</Text>
             </View>
-
-            <TouchableOpacity className="bg-[#B390EC] h-10 w-48 mb-1 mt-1 rounded-full shadow-xl shadow-gray-400 items-center align-middle">
-              <Text className="text-l font-semibold pt-2">Ramasser le déchet</Text>
+            <View className="flex-row">
+            <TouchableOpacity className="flex-1 bg-[#B390EC] h-8 w-35 mb-1 mt-1 mr-1 rounded-full shadow-xl shadow-gray-400 items-center align-middle">
+              <Text className="text-l font-semibold pt-2">Ramasser</Text>
             </TouchableOpacity>
+
+            <TouchableOpacity className="flex-1  bg-[#ed5564] h-8 w-39 mb-1 mt-1 mr-1 rounded-full shadow-xl shadow-gray-400 items-center align-middle">
+              <Text className="text-l font-semibold pt-2">Ramassage impossible</Text>
+            </TouchableOpacity>
+            </View>
+            
           </View>
         </Callout>
       </Marker>
-    </MapView>
 
+      <View style={{alignSelf: 'flex-end'}} className="absolute bottom-10 mr-1" >
+        <TouchableOpacity style={{alignSelf: 'flex-end'}} 
+          className="absolute bottom-10 bg-gray-200 rounded-full items-center drop-shadow-2xl"
+          onPress={(e) =>{
+            console.log("hey")
+            latitude= e.nativeEvent.coordinate.latitude
+            longitude= e.nativeEvent.coordinate.longitude
+          }} >
+            <ArrowsPointingInIcon size={50} color="#000"/>
+        </TouchableOpacity>
+      </View>
+
+    </MapView>
+    
   )
 }
 
--- components/Report.js
+++ components/Report.js
@@ -1,7 +1,9 @@
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import { StyleSheet, Text, View, Image, SafeAreaView, Button, StatusBar } from 'react-native'
+import React, { useLayoutEffect, useEffect, useRef, useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
-//import ImagePicker from 'react-native-image-crop-picker';
+import { Camera } from 'expo-camera';
+import { shareAsync } from 'expo-sharing';
+import * as MediaLibrary from 'expo-media-library';
 
 
 const Report = () => {
@@ -14,43 +16,113 @@ const Report = () => {
     })
   }, []);
 
-  // ImagePicker.openPicker({
-  //   width: 300,
-  //   height: 400,
-  //   cropping: true
-  // }).then(image => {
-  //   console.log(image);
-  // });
+  let cameraRef = useRef();
+  const [hasCameraPermission, setHasCameraPermission] = useState();
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
+  const [photo, setPhoto] = useState();
 
-  return (
-    <SafeAreaView className="h-full bg-white pt-5">
-      <Text>
-        {/* Header */}
-        <View className="flex-row pb-2 items-center mx-4 space-x-2 pt-5 ">
-          <Image className=" flex-1 h-11 w-11 p-4" source={require('./img/logo_cote.png')} />
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+      setHasCameraPermission(cameraPermission.status === "granted");
+      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+    })();
+  }, []);
 
-          <View className="flex-row mx-4 ">
-            <Text className="font-bold text-gray-400 text-sm">Localisation : Rouen </Text>
+  if (hasCameraPermission === undefined) {
+    return <Text>Requesting permissions...</Text>
+  } else if (!hasCameraPermission) {
+    return <Text>Permission for camera not granted. Please change this in settings.</Text>
+  }
+
+  let takePic = async () => {
+    let options = {
+      quality: 1,
+      base64: true,
+      exif: true
+    };
+
+    let newPhoto = await cameraRef.current.takePictureAsync(options);
+    setPhoto(newPhoto);
+  };
+
+  if (photo) {
+    let sharePic = () => {
+      shareAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+    let savePhoto = () => {
+      MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+
+
+    return (
+      <SafeAreaView className="h-full bg-white pt-5">
+        <Text>
+          {/* Header */}
+          <View className="flex-row pb-2 items-center mx-4 space-x-2 pt-5 ">
+            <Image className=" flex-1 h-11 w-11 p-4" source={require('./img/logo_cote.png')} />
+
+            <View className="flex-row mx-4 ">
+              <Text className="font-bold text-gray-400 text-sm">Localisation : Rouen </Text>
+            </View>
+
+            <View className=" mx-4 ">
+              <Image className="h-8 w-8 bg-gray-300 p-4 mx-4 rounded-full" source={{ url: 'https://links.papareact.com/wru' }} />
+              <Text className="font-bold text-gray-400 text-xs">oumss78 </Text>
+            </View>
+
           </View>
+        </Text>
 
-          <View className=" mx-4 ">
-            <Image className="h-8 w-8 bg-gray-300 p-4 mx-4 rounded-full" source={{ url: 'https://links.papareact.com/wru' }} />
-            <Text className="font-bold text-gray-400 text-xs">oumss78 </Text>
-          </View>
-
-        </View>
-      </Text>
 
         {/* Camera */}
-        <View className="h-full items-center">  
+        <View className="h-full flex-1 items-center justify-center">
           <Text className=" font-semibold text-lg" >Signaler un déchet</Text>
           <Text>Camera Zone</Text>
+
+          <Image className="self-stretch flex-1" source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
+          <Button title="Share" onPress={sharePic} />
+          {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined}
+          <Button title="Discard" onPress={() => setPhoto(undefined)} />
         </View>
 
-    </SafeAreaView>
-  )
+      </SafeAreaView>
+    )
+  }
+
+return (
+  <SafeAreaView className="h-full bg-white pt-5">
+    <Text>
+      {/* Header */}
+      <View className="flex-row pb-2 items-center mx-4 space-x-2 pt-5 ">
+        <Image className=" flex-1 h-11 w-11 p-4" source={require('./img/logo_cote.png')} />
+
+        <View className="flex-row mx-4 ">
+          <Text className="font-bold text-gray-400 text-sm">Localisation : Rouen </Text>
+        </View>
+
+        <View className=" mx-4 ">
+          <Image className="h-8 w-8 bg-gray-300 p-4 mx-4 rounded-full" source={{ url: 'https://links.papareact.com/wru' }} />
+          <Text className="font-bold text-gray-400 text-xs">oumss78 </Text>
+        </View>
+
+      </View>
+    </Text>
+    <Camera className="flex-1 items-center justify-center" ref={cameraRef}>
+      <View className="bg-white" style={{ alignSelf: 'flex-end' }} >
+        <Button title="Take Pic" onPress={takePic} />
+      </View>
+      <StatusBar style="auto" />
+    </Camera>
+  </SafeAreaView>
+
+)
 }
-
-export default Report
-
-const styles = StyleSheet.create({})
+  export default Report
